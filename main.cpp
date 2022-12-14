0a64a1f9284a8c55ecfd649b6744f9793c28309a
Charlotte Thomas <contact@nwa2coco.fr>
2022-12-14 13:26:42 +0100

    deleted .idea add tokens

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+
+enum Token {
+    token_eof = -1,
+
+    token_def = -2,
+    token_extern = -3,
+
+    token_identifier = -4,
+    token_number = -5,
+
+};
+
+static std::string IdentifierStr;
+static double numval;
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
