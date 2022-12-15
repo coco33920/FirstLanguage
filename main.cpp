9e6a40f515758122cea8b65ec0e60b304c82935a
Charlotte Thomas <contact@nwa2coco.fr>
2022-12-15 12:11:22 +0100

    basic lexer

--- main.cpp
+++ main.cpp
@@ -15,8 +15,51 @@ enum Token {
 static std::string IdentifierStr;
 static double numval;
 
+static int getTokken() {
+    static int lastChar = ' ';
+    while (isspace(lastChar))
+        lastChar = getchar();
+
+    if(isalpha(lastChar)){
+        IdentifierStr = lastChar;
+        while (std::isalnum(lastChar = getchar()))
+            IdentifierStr += lastChar;
+        if (IdentifierStr == "def")
+            return token_def;
+        if(IdentifierStr == "extern")
+            return token_extern;
+        return token_identifier;
+    }
+
+    if(std::isdigit(lastChar) || lastChar == '.'){
+        std::string numStr;
+        do {
+            numStr += lastChar;
+            lastChar = getchar();
+        } while (std::isdigit(lastChar) || lastChar == '.');
+        numval = strtod(numStr.c_str(),0);
+        return token_number;
+    }
+
+    if (lastChar == '#') {
+        do
+            lastChar = getchar();
+        while (lastChar != EOF && lastChar != '\n' && lastChar != '\r');
+
+        if (lastChar != EOF)
+            return getTokken();
+    }
+    if (lastChar == EOF)
+        return token_eof;
+
+    int ThisChar = lastChar;
+    lastChar = getchar();
+    return ThisChar;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int s = getTokken();
+    std::cout << s << std::endl;
     return 0;
 }
